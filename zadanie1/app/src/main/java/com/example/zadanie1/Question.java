ecc97e1db77401286bc2328322fac36bd3a9b443
lewy212 <huak@wp.pl>
2023-10-18 23:01:57 +0200

    Add new files

---
+++ zadanie1/app/src/main/java/com/example/zadanie1/Question.java
@@ -0,0 +1,23 @@
+package com.example.zadanie1;
+
+public class Question {
+    private int questionId;
+    private boolean trueAnswer;
+
+    public Question(int questionId, boolean trueAnswer) {
+        this.questionId = questionId;
+        this.trueAnswer = trueAnswer;
+    }
+
+    public int getQuestionId() {
+        return questionId;
+    }
+
+    public boolean isTrueAnswer() {
+        return trueAnswer;
+    }
+
+    public boolean isTrue() {
+        return trueAnswer;
+    }
+}
